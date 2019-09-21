b31b64bf38c9ca6944bb60daae3a248452be0b82
Adam Bremler <adambremler@hotmail.com>
2019-09-21 14:05:29 +0200

    Add route for getting and creating URL by URL

--- routes/urls.js
+++ routes/urls.js
@@ -31,4 +31,76 @@ router.get('/:id', async (req, res) => {
     });
 });
 
+router.get('/', async (req, res) => {
+    const inputURL = req.query.url;
+
+    if (!inputURL) {
+        return res.status(400).json({ error: 'No URL received' });
+    }
+
+    urlToSave = normalizeUrl(inputURL, {
+        stripHash: true,
+        stripProtocol: true
+    });
+
+    // Better solution later, JSON to prevent differences in parameter order?
+    const existingURL = await URLModel.findOne({ url: urlToSave });
+
+    if (existingURL) {
+        return res.json({
+            url: {
+                id: existingURL.id,
+                url: existingURL.url,
+                screenshotPath: existingURL.screenshotPath
+            }
+        });
+    }
+
+    normalizedURL = normalizeUrl(urlToSave);
+
+    if (!(await isReachable(normalizedURL))) {
+        return res.status(400).json({ error: 'Invalid URL' });
+    }
+
+    let screenshotPath = null;
+
+    do {
+        const screenshotID = uuidv4();
+
+        screenshotPath = `images/urls/${screenshotID}.png`;
+    } while (await URLModel.findOne({ screenshotPath }));
+
+    const browser = await puppeteer.launch({
+        defaultViewport: {
+            width: 1280,
+            height: 720,
+            deviceScaleFactor: 0.5
+        }
+    });
+
+    const page = await browser.newPage();
+    await page.goto(normalizedURL);
+    await page.screenshot({ path: `public/${screenshotPath}` });
+
+    await browser.close();
+
+    await new URLModel({
+        url: urlToSave,
+        screenshotPath
+    })
+        .save()
+        .then(url => {
+            return res.status(201).json({
+                url: {
+                    id: url.id,
+                    url: url.url,
+                    screenshotPath: url.screenshotPath
+                }
+            });
+        })
+        .catch(() => {
+            res.status(500).json({ error: 'An error occurred' });
+        });
+});
+
 module.exports = router;
